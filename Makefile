d9dd6db6b926c6862ac7a3f70945855e26888829
Daniel MacDougall <dmacdougall@gmail.com>
2018-04-25 04:50:53 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,33 @@
+BUILD := build
+SRC := $(wildcard *.c)
+INC := $(wildcard *.h)
+OBJ := $(SRC:%.c=$(BUILD)/%.o)
+BIN := $(BUILD)/example
+
+CFLAGS := -std=c99 -O2 -g -Wall -Werror -pedantic -F lib
+LDFLAGS := -F lib -framework SDL2
+
+.DEFAULT_GOAL := $(BIN)
+
+$(BIN): $(OBJ)
+	$(CC) -o $@ $^ $(LDFLAGS)
+	install_name_tool -add_rpath lib $@
+
+$(BUILD)/%.o: %.c | $(BUILD)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+$(BUILD)/depfile: $(SRC) $(INC) | $(BUILD)
+	$(CC) -MM $(SRC) $(CFLAGS) | sed -e 's/^/$(BUILD)\//' > $@
+
+$(BUILD):
+	mkdir -p $@
+
+-include $(BUILD)/depfile
+
+.PHONY: clean
+clean:
+	rm -fr $(BUILD)
+
+.PHONY: run
+run: $(BIN)
+	./$(BIN)
---
+++ example.c
@@ -0,0 +1,53 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#include <SDL2/SDL.h>
+
+int main(void) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL_Init: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Example",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        750,
+        1334,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_INPUT_FOCUS);
+    if (window == NULL) {
+        printf("SDL_CreateWindow: %s", SDL_GetError());
+        return 1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) {
+        printf("SDL_CreateRenderer: %s", SDL_GetError());
+        return 1;
+    }
+
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event = {0};
+        while (SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                    quit = true;
+                    break;
+                }
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+    return 0;
+}
